d2d8f6210d42cc7f6c2816c1bec2775b44f09999
Marinidis Antonios <antonis.marinidis7@gmail.com>
2022-12-13 13:29:21 +0100

    final model ready for presentation

--- firstTest/sarima.py
+++ firstTest/sarima.py
@@ -44,7 +44,7 @@ def differencing_parameter_seasonal(df):
 
 kafsimo = 'unleaded_95'
 nomos = 'ΝΟΜΟΣ_ΑΡΤΗΣ'
-periodos = 180
+periodos = 90
 
 print(kafsimo)
 print(nomos)
@@ -62,9 +62,6 @@ df = df[["record_date", kafsimo]]
 df["record_date"] = pd.to_datetime(df["record_date"], dayfirst=True)
 df = df.sort_values(by=['record_date'])
 
-second_diff = adfuller(df[kafsimo].diff(12).dropna())
-print(second_diff[1])
-
 # fill the missing dates of the dataframe
 dm = df
 dm.set_index('record_date', inplace=True)
@@ -79,29 +76,31 @@ dm.interpolate(method ='linear', limit_direction ='forward', inplace=True)
 dh = dm.asfreq('M') #for daily resampled data and fillnas with appropriate method
 decomposition = sm.tsa.seasonal_decompose(dh[kafsimo], model='additive', 
                             extrapolate_trend='freq') #additive or multiplicative is data specific
-fig = decomposition.plot()
-plt.show()
 
-seasonality=decomposition.seasonal
-seasonality.plot(color='green')
-plt.show()
+# fig = decomposition.plot()
+# plt.show()
+
+# seasonality=decomposition.seasonal
+# seasonality.plot(color='green')
+# plt.show()
 
 #####################################################################################################
 #### PREVIOUS PREDICTION ####
 
 t_dm = dm
-split_value = dm.size - periodos
+split_value = len(dm[kafsimo]) - periodos
 training_dm = dm.iloc[:split_value, :]
 
 e_d = differencing_parameter(training_dm)
 e_dD = differencing_parameter_seasonal(training_dm)
 
 existing_model=sm.tsa.statespace.SARIMAX(training_dm[kafsimo],order=(1, e_d, 1),seasonal_order=(6,e_dD,1,12))
+# existing_model=sm.tsa.arima.ARIMA(training_dm[kafsimo],order=(1, e_d, 1))
 existing_results = existing_model.fit()
 
-t_dm['forecast'] = existing_results.predict(start = split_value, end = t_dm.size, dynamic= True)  
-t_dm[[kafsimo, 'forecast']].plot(figsize=(12, 8))
-plt.show()
+dm['existing_forecast'] = existing_results.predict(start = split_value, end = len(dm[kafsimo]), dynamic= True)  
+# dm[[kafsimo, 'existing_forecast']].plot(figsize=(12, 8))
+# plt.show()
 
 
 #####################################################################################################
@@ -110,6 +109,7 @@ plt.show()
 f_d = differencing_parameter(dm)
 f_dD = differencing_parameter_seasonal(dm)
 future_model=sm.tsa.statespace.SARIMAX(dm[kafsimo],order=(1, f_d, 1),seasonal_order=(6,f_dD,1,12))
+# future_model=sm.tsa.arima.ARIMA(dm[kafsimo],order=(1, f_d, 1))
 future_results = future_model.fit()
 
 # Making a NAN value future dataset
@@ -117,8 +117,18 @@ pred_date = [dm.index[-1]+ DateOffset(days=x) for x in range(0,periodos)]
 pred_date = pd.DataFrame(index=pred_date[1:],columns=dm.columns)
 
 data=pd.concat([dm,pred_date])
-p_start = dm.size
-p_end = dm.size + periodos
-data['forecast'] = future_results.predict(start = p_start, end = p_end, dynamic= True)  
-data[[kafsimo, 'forecast']].plot(figsize=(12, 8))
+p_start = len(dm[kafsimo])
+p_end = len(dm[kafsimo]) + periodos
+data['future_forecast'] = future_results.predict(start = p_start, end = p_end, dynamic= True) 
+
+# data[[kafsimo, 'future_forecast']].plot(figsize=(12, 8))
+# plt.show()
+
+# fig1=plot_acf(dm[kafsimo].diff().dropna())
+# fig2=plot_pacf(dm[kafsimo].diff().dropna())
+
+# fig1=plot_acf(dm[kafsimo].diff(12).dropna())
+# fig2=plot_pacf(dm[kafsimo].diff(12).dropna())
+
+data[[kafsimo, 'future_forecast', 'existing_forecast']].plot(figsize=(12, 8))
 plt.show()
