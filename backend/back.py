d630f5a88472bc63610f22a47b630c540e981a7a
Marinidis Antonios <antonis.marinidis7@gmail.com>
2022-12-16 14:17:41 +0100

    get the prices table and convert it t dataframe

--- backend/back.py
+++ backend/back.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import mysql.connector as mariadb
 import connection_properties as conn
-import connection
+# import connection
+import pandas as pd
 
 # cursor = connection.connection()
 
@@ -10,12 +11,20 @@ mariadb_connection = mariadb.connect(user=conn.USER,
                                     host=conn.HOST,
                                     database=conn.DATABASE,
                                     auth_plugin='mysql_native_password')
-                                    
+
 cursor = mariadb_connection.cursor()
 
-sql = "SELECT * FROM prices"
+sql = "SELECT * FROM thesis.prices"
 cursor.execute(sql)
 records = cursor.fetchall()
+
+sql1 = """SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'prices'"""
+cursor.execute(sql1)
+column_names = cursor.fetchall()
+print(column_names)
+
+df = pd.DataFrame(records, columns=column_names)
+print(df)
 
 app = FastAPI()
 
--- backend/connection.py
+++ backend/connection.py
@@ -1,17 +1,17 @@
 import mysql.connector as mariadb
 import connection_properties as conn
 
-# def connection():
-mariadb_connection = mariadb.connect(user=conn.USER,
-                                    password=conn.PASSWORD,
-                                    host=conn.HOST,
-                                    database=conn.DATABASE,
-                                    auth_plugin='mysql_native_password')
-cursor = mariadb_connection.cursor()
-    # return cursor
+def connection():
+    mariadb_connection = mariadb.connect(user=conn.USER,
+                                        password=conn.PASSWORD,
+                                        host=conn.HOST,
+                                        database=conn.DATABASE,
+                                        auth_plugin='mysql_native_password')
+    cursor = mariadb_connection.cursor()
+    return cursor
 
 # cursor = connection()
-sql = "SELECT * FROM prices"
-cursor.execute(sql)
-records = cursor.fetchall()
-print("Total number of rows in table: ", cursor.rowcount)
+# sql = "SELECT * FROM prices"
+# cursor.execute(sql)
+# records = cursor.fetchall()
+# print("Total numberascascasca of rows in table: ", cursor.rowcount)
